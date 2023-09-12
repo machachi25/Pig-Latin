b69df41263f7cb1a5831d0cd559411be88539492
Chinmay Machavaram <machachi25@7hills.org>
2023-09-11 20:39:35 -0400

    Started from fresh, as some stuff didnt save

--- PigLatin.java
+++ PigLatin.java
@@ -4,14 +4,15 @@ public class PigLatin
 {
     public static void main(String [] args)
     {
-        String word = "chin";
-        int len = word.length();
-        for(int i = 0; i < len; i++)
+        String phrase;
+        int len;
+        System.out.println("Enter a word of your choosing: ");
+        Scanner scan = new Scanner(System.in);
+        phrase = scan.nextLine();
+        len = phrase.length;
+        for(int i =0; i < len; i++)
         {
-            if(word[i].charAt("a"))
-            word.substring(i, i + 1); // up to but not including i + 1
+            phrase.substring(i, i+1);
         }
-        //word[i];
-        //rules of pig latin: 
     }
 }
