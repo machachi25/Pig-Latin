abca330edb77d615ee9ebf1ee60866901b2d36c8
Chinmay Machavaram <machachi25@7hills.org>
2023-09-11 20:54:29 -0400

    Attempting to move letters of word

--- PigLatin.java
+++ PigLatin.java
@@ -4,13 +4,15 @@ public class PigLatin
 {
     public static void main(String [] args)
     {
+       //Rules of pig latin: put last consonant or consonant cluster at the beginning and add "ay" to the end of the word (e.g. - pig --> igpay)
         String phrase;
         int len;
         System.out.println("Enter a word of your choosing: ");
         Scanner scan = new Scanner(System.in);
         phrase = scan.nextLine();
-        len = phrase.length;
-        for(int i =0; i < len; i++)
+        len = phrase.length();
+        String temp;
+        for(int i = 1; i < len; i++)
         {
             phrase.substring(i, i+1);
         }
